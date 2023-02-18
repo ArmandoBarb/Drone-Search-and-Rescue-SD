8b0ac4ce6e3151bb331feac80b00567ae5922294
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2023-02-18 13:28:43 -0500

    Add files via upload

--- needsIntegration/depthDataCollection.py
+++ needsIntegration/depthDataCollection.py
@@ -32,17 +32,14 @@ lowDepth = 0
 imageContainer = []
 
 # image collection loop:
+# we have to test out how many pictures it should take so the while loop can end
 while i < 1:
 
     # take images
     # ImageRequest(name, image_type, pixel_as_float, compress)
-    response = client.simGetImages(
-        [airsim.ImageRequest("0", airsim.ImageType.Scene,False, False),
-        airsim.ImageRequest("0", airsim.ImageType.DepthVis, True)],
-        vehicle_name)
+    response = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.DepthVis, True)],vehicle_name)
 
-    responseScene = response[0]
-    responseDepth = response[1]
+    responseDepth = response[0]
 
     depth = np.array(responseDepth.image_data_float,dtype=np.float32)
     depth = depth.reshape(responseDepth.height,responseDepth.width)
@@ -51,9 +48,12 @@ while i < 1:
     depthCloce16 = np.clip(depthClose,0,65535)
     os.chdir(imgDir)
     cv2.imwrite('DepthImage.png', depthCloce16)
+
+    # we split the image into multiple parts(3 col 1 row)
     split_image('DepthImage.png',1,3,False,False)
     files = os.listdir(imgDir)
 
+    # we find the average pixels in the image and we store it in the imageContainer
     for images in files:
         img = cv2.imread(images)
         temp = np.average(img)
@@ -63,24 +63,25 @@ while i < 1:
             lowDepth = temp
             imageContainer.append(images)
 
+    # this is converting the pixels into meters converstion 
     picDiviation = cv2.imread(imageContainer[0])
-
     originPicWidth = depthCloce16.shape[1]
     width = picDiviation.shape[1]
     centerDrone = width/2
     width = originPicWidth + width + centerDrone
     meterConvertion = ((width*24000)*0.000265)+1
 
-    # print(meterConvertion)
+    
     distanceFromObsitcal = client.getDistanceSensorData(vehicle_name=vehicle_name).distance
     droneData = client.getGpsData()
     lidarData = client.getLidarData()
     
+    # we have to play with this moveByVelo thing because for the collision algo to work the drone has to be moving 
     client.moveByVelocityZAsync(5,0, -5, duration =2,vehicle_name=vehicle_name).join()
-    # time.sleep(1)
 
     velocity = client.getGpsData(vehicle_name = vehicle_name)
 
+    # math to find the x and y values to find the vectors
     theta = math.atan2(distanceFromObsitcal,meterConvertion)/math.pi*180
     cs = math.cos(theta)
     sn = math.sin(theta)
@@ -100,34 +101,6 @@ while i < 1:
     client.moveByVelocityZAsync(pyNormalized,pxNormalized, -5, duration =1,vehicle_name=vehicle_name)
     print('works')
 
-    # print('------------Lidar Information --------------')
-    # print(lidarData.pose.position.x_val)
-    # print(lidarData.pose.position.y_val)
-    # print(lidarData.pose.position.z_val)
-
-    # if(droneData.longitude > 0):
-    #     x = droneData.longitude + longitudeConvertion
-    # else:
-    #     x = droneData.longitude - longitudeConvertion
-
-    # if(droneData.latitude > 0):
-    #     y = droneData.latitude + latitudeConverstion
-    # else:
-    #     y = droneData.latitude - latitudeConverstion
-
-    # xDifference = float(meterConvertion) - lidarData.pose.position.x_val
-    # # print(xDifference)
-    # yDifference = float(distanceFromObsitcal) - lidarData.pose.position.y_val
-    # # print(droneData.latitude)
-    # # print(yDifference)
-
-    # xNormalized = (xDifference / sqrt(xDifference**2 + yDifference**2))*DIRECTION_FACTOR
-    # yNormalized = (yDifference / sqrt(xDifference**2 + yDifference**2))*DIRECTION_FACTOR
-
-    # print(xNormalized)
-    # print(yNormalized)
-
-    # client.moveByVelocityZAsync(yNormalized,xNormalized, 0, duration =1,vehicle_name=vehicle_name)
 
     time.sleep(1)
     i+=1
